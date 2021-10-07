831d1c910e43457d6c64f2f940ee279cd9f58895
saeki.satoshi <saeki.satoshi@tis.co.jp>
2021-10-07 13:17:58 +0900

    Update setup.py for project descriotion on PyPI site

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup
 setup(
     name="httpie-oauth2-client-credentials",
     description="httpie auth plugin for OAuth2.0 client credentials flow.",
-    version="0.1.0",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    version="0.1.1",
     author='satdoc',
     author_email='satodoc-develop-public@outlook.com',
+    license='MIT',
     url='https://github.com/satodoc/httpie-oauth2-client-credentials',
     download_url='https://github.com/satodoc/httpie-oauth2-client-credentials',
     py_modules=['httpie_oauth2_client_credentials'],
@@ -14,5 +17,15 @@ setup(
         'httpie.plugins.auth.v1': [
             'httpie_oauth2_client_credentials = httpie_oauth2_client_credentials:OAuth2ClientCredentialsPlugin'
         ]
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ],
 )
