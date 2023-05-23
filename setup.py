6db687ed9a1a3eb9d5f9d9f3298783072c367e27
satodoc <70416812+satodoc@users.noreply.github.com>
2023-05-23 14:58:36 +0900

    Upgraded the required version of httpie to resolve dependency issues with urllib

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/satodoc/httpie-oauth2-client-credentials',
     download_url='https://github.com/satodoc/httpie-oauth2-client-credentials',
     py_modules=['httpie_oauth2_client_credentials'],
-    install_requires=['httpie>=2.0.0'],
+    install_requires=['httpie>=3.2.2'],
     entry_points={
         'httpie.plugins.auth.v1': [
             'httpie_oauth2_client_credentials = httpie_oauth2_client_credentials:OAuth2ClientCredentialsPlugin'
